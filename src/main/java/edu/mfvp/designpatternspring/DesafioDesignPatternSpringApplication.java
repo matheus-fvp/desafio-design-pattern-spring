063975cfbcab66c5ba9be004641054ada4954497
Matheus Fernando Vieira Pinto <vieira.matheusf13@gmail.com>
2023-03-28 15:52:01 -0300

    first commit

---
+++ src/main/java/edu/mfvp/designpatternspring/DesafioDesignPatternSpringApplication.java
@@ -0,0 +1,13 @@
+package edu.mfvp.designpatternspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDesignPatternSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDesignPatternSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/mfvp/designpatternspring/DesafioDesignPatternSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mfvp.designpatternspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDesignPatternSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
